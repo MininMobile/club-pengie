4cb06a5640f91db299675d20f159c6a5212f9952
MininMobile <miningamer@gmail.com>
2017-12-21 20:46:25 +0000

    Add background

--- assets/run.js
+++ assets/run.js
@@ -2,4 +2,4 @@ const cvg = require("convergejs");
 const canvas = document.getElementById("canvas");
 const c = new cvg.Canvas(canvas, "600px", "450px", 30);
 
-c.style([["background", "black"]]);
+c.style([["background", "url('home.png')"]]);
