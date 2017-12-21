9e464f9a12dcf8ff3233f944201f9a29cbb59583
MininMobile <miningamer@gmail.com>
2017-12-21 22:01:10 +0000

    Add player

--- assets/run.js
+++ assets/run.js
@@ -3,3 +3,8 @@ const canvas = document.getElementById("canvas");
 const c = new cvg.Canvas(canvas, "600px", "450px", 30);
 
 c.style([["background", "url('home.png')"]]);
+const p = c.new("box", "player");
+
+p.size({ height:"100px", width:"65px" });
+p.style([["background", "url('pengoi.png')"], ["background-size", "contain"], ["transition", "all 1s"]]);
+p.move({ y:"200px" });
