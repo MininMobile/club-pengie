33f1898fdc112612582cc9e714dc2bee5f1e09f1
MininMobile <miningamer@gmail.com>
2017-12-21 22:37:37 +0000

    MOVE

--- assets/run.js
+++ assets/run.js
@@ -8,3 +8,15 @@ const p = c.new("box", "player");
 p.size({ height:"100px", width:"65px" });
 p.style([["background", "url('pengoi.png')"], ["background-size", "contain"], ["transition", "all 1s"]]);
 p.move({ y:"200px" });
+
+document.onmousedown = (event) => {
+    if ((event.x < c.getSize().x-p.getSize().x/2)
+        && (event.x > p.getSize().x/2)
+        && (event.y < 385)
+        && (event.y > 225)) {
+        p.move({
+            x:(event.x)-p.getSize().x/2 + "px",
+            y:(event.y)-p.getSize().y + "px"
+        });
+    }
+}
